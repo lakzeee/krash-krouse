acf250d596bd15e2f1df3416ff80e8332e696540
lakzeee <caizh1997@live.com>
2025-06-22 15:24:15 -0500

    chore: add config-vitest pkg

--- apps/client/vitest.config.unit.ts
+++ apps/client/vitest.config.unit.ts
@@ -1,34 +1,3 @@
-// vitest.config.unit.ts
-import { coverageConfigDefaults, defineConfig } from "vitest/config";
+import config from '@repo/config-vitest/next';
 
-export default defineConfig({
-  test: {
-    include: ["**/*.test.ts"],
-    coverage: {
-      provider: "v8",
-      enabled: true,
-      exclude: [
-        "**/*.config.ts",
-        "**/*.config.mjs",
-        "**/*.config.js",
-        "prisma/generated/**",
-        "**/zod.ts",
-        "**/prisma.ts",
-        "components/ui/**",
-        "**/index.ts",
-        ...coverageConfigDefaults.exclude,
-      ],
-    },
-  },
-  resolve: {
-    alias: {
-      "@/app": "/app",
-      "@/lib": "/lib",
-      "@/prisma": "/prisma",
-      "@/services": "/services",
-      "@/store": "/store",
-      "@/types": "/types",
-      "@/utils": "/utils",
-    },
-  },
-});
+export default config;
